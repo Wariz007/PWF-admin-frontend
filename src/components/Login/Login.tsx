96ea52646184aa8fb8ab7a5337284b96d65b3d2a
wariz <bookbodmas31@gmail.com>
2025-11-08 21:29:54 +0100

    used ec2 public ip

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -10,7 +10,7 @@ function Login({ onLoginSuccess }: { onLoginSuccess: (token: string) => void }) 
     setError("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("http://13.61.21.78:5000/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
